6c9024bbbad002337823bb506f0a9a1ee86bcd58
EliasWollitzer <elias.wollitzer@htl-braunau.at>
2017-12-12 10:46:36 +0100

    Server Packages

--- Node_Server/Server.js
+++ Node_Server/Server.js
@@ -1,15 +1,24 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var mysql = require('mysql');
-
+var express = require("express");
+var mysql = require("mysql");
+var path = require("path");
 var bodyParser = require("body-parser");
+var expressValidator = require("express-validator");
+var urlencodedParser = bodyParser.urlencoded({extendet: true});
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
-  })); 
-  eine geänderte änderung
+  }));
+   
+  app.use(function (req, res, next) { // Test
+    console.log('Time:', Date.now());
+    next();
+  });
+
+app.get('/hello', function(req, res){// pfad localhost:8080/hello
+    res.send("Hello World");
+});
 /*
 var con = mysql.createConnection({
     host:'localhost',
