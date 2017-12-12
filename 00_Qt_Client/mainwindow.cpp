27cbfae90539d18fa52da372b3eb4819452b2c5a
FM <emailadresse57@gmail.com>
2017-12-12 11:33:32 +0100

    QT

--- 00_Qt_Client/mainwindow.cpp
+++ 00_Qt_Client/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +12,47 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_calendarWidget_clicked(const QDate &date)
+{
+    QString d;
+    d = QString::number(date.year()) + "-" + QString::number(date.month()) + "-" + QString::number(date.day());
+    qDebug()<<"on_calendarWidget_clicked: "<< d;
+
+    http_get_list(d);
+}
+
+void MainWindow::http_get_list(QString date){
+
+
+    QEventLoop eventLoop;
+    QUrl localURL(QString("http://10.8.250.42:30000/sql_get"));
+    QNetworkAccessManager mgr;
+
+    QUrlQuery qu;
+    qu.addQueryItem("date", date.toLatin1());
+    qDebug() << "http_get_list: " << qu.toString();
+    localURL.setQuery(qu);
+    QNetworkRequest request(localURL);
+
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    QNetworkReply *reply = mgr.get(request); // GET
+
+
+
+    connect(reply, SIGNAL(finished()), &eventLoop, SLOT(quit()));
+    connect(reply, SIGNAL (finished()), this , SLOT(get_init()));
+
+
+    eventLoop.exec();
+}
+
+void MainWindow::get_init(){
+    QNetworkReply* reply = qobject_cast<QNetworkReply*>(sender());
+
+    QByteArray response_data = reply->readAll();
+    //reply->deleteLater();
+    QJsonDocument json = QJsonDocument::fromJson(response_data);
+    qDebug() << "get_init: " << json.toJson();
 }
--- 00_Qt_Client/mainwindow.h
+++ 00_Qt_Client/mainwindow.h
@@ -2,6 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtCore>
+#include <QString>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QEventLoop>
+#include <QUrlQuery>
+#include <QUrl>
+#include <QByteArray>
+#include <QJsonDocument>
+#include <QJsonArray>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +25,11 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_calendarWidget_clicked(const QDate &date);
+    void http_get_list(QString date);
+    void get_init();
 
 private:
     Ui::MainWindow *ui;
